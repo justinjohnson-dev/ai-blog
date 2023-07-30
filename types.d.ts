088bbae143392eb82b30f0037afdd972a452d30e
Justin Johnson <jjustin634@gmail.com>
2023-07-30 14:28:00 -0500

    Dave gray remote mdx tutorial (#2)
    * setting up using git API following Dave Gray setup
    
    * updating lint script
    
    * adding sitemap, and slugs for dynamic routes

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+type Meta = {
+    id: string;
+    title: string;
+    date: string;
+    tags: string[];
+  };
+  
+  type BlogPost = {
+    meta: Meta;
+    content: ReactElement<any, string | JSXElementConstructor<any>>
+  };
+  
