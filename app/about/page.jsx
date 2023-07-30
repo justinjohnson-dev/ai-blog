baf8b53211d03148b15380a7b43d40a7eef276c3
Justin Johnson <jjustin634@gmail.com>
2023-07-30 15:40:39 -0500

    adding tailwindcss to home page and adding about page (#3)

---
+++ app/about/page.jsx
@@ -0,0 +1,39 @@
+export default function About() {
+  return (
+    <div className="justify-center items-center m-10">
+      <h1 className="text-4xl font-bold text-center mb-5 mt-12">
+        About Our{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          AI Blog
+        </span>
+      </h1>
+
+      <p className="text-lg mb-5 text-center">
+        Welcome to our unique blog application run by AI and specifically,
+        GPT-4. This state-of-the-art AI generates fresh, relevant, and
+        intriguing content each day focusing on technology-related topics.
+      </p>
+
+      <p className="text-lg mb-5 text-center">
+        The content spans a wide variety of themes in the tech world, from
+        artificial intelligence, data science, web development, to cyber
+        security and much more. Each article features tags for related articles,
+        allowing you to explore the technology world in an interconnected
+        manner.
+      </p>
+
+      <p className="text-lg mb-5 text-center">
+        The entire application is built using a modern and efficient stack,
+        including Next.js, React, Tailwind CSS, and next-mdx-remote. This
+        guarantees a smooth, interactive, and aesthetically pleasing user
+        experience.
+      </p>
+
+      <p className="text-lg mb-5 text-center">
+        Whether you're a seasoned tech enthusiast or just starting your journey
+        in the field, our blog is a source of invaluable knowledge and insights.
+        Enjoy exploring!
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,15 @@ export const revalidate = 86400;
 export default function Home() {
   return (
     <div className="mx-auto">
-      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        Hello and welcome!
-        <span className="whitespace-nowrap">
-          <span className="font-bold">👋 JJ</span>
-        </span>
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl mt-12">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Welcome
+        </span>{" "}
+        to the AI Tech Blog.
+      </h1>
+      <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+        Discover the future today with our AI-driven blog! Cutting-edge insights
+        on tech, powered by GPT-4, delivered daily.
       </p>
       <Posts />
     </div>
