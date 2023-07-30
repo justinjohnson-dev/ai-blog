7234eec69ada1c4c19d2336b5c7f22dbc8bd24b9
Justin Johnson <jjustin634@gmail.com>
2023-07-30 15:40:00 -0500

    adding tailwindcss to home page and adding about page

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,21 +1,21 @@
-import { getPostsMeta } from '@/lib/post';
-import ListItem from './ListItem';
+import { getPostsMeta } from "@/lib/post";
+import ListItem from "./ListItem";
 
 export default async function Posts() {
-    const posts = await getPostsMeta();
+  const posts = await getPostsMeta();
 
-    if (!posts) {
-        return <p className='mt-10 text-center>'>No posts found.</p>
-    };
+  if (!posts) {
+    return <p className="mt-10 text-center>">No posts found.</p>;
+  }
 
-    return (
-        <section className='mt-6 mx-auto max-w-2xl'>
-            <h2 className='text-4xl font-bold dark:text-white/90'>Blog</h2>
-            <ul className='w-full list-none p-0'>
-                {posts.map((post) => (
-                    <ListItem key={post.id} post={post} />
-                ))}
-            </ul>
-        </section>
-    ) 
+  return (
+    <section className="mt-6 mx-auto max-w-2xl">
+      <h2 className="text-4xl font-bold dark:text-white/90">Articles</h2>
+      <ul className="w-full list-none p-0">
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </section>
+  );
 }
